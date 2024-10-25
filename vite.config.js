682a428f6e301c2716ed0103e09c70c5261efdb5
TanujaGajavelli <tanujasai20@gmail.com>
2024-10-25 15:15:26 +0530

    burberry added

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/burberry-6nxj1tgxn-tanujagajavellis-projects.vercel.app',
   plugins: [react()],
 });
 
